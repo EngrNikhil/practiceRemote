f02b2a73d688f9558eaecd94644bfb0272025308
Qa-Nikhil <Ernikhil.jadhav2306@gmail.com>
2023-01-22 16:40:43 +0530

    added i value in practice 01 class

--- PractiseNj/src/main/java/practice01.java
+++ PractiseNj/src/main/java/practice01.java
@@ -1,9 +1,11 @@
 public class practice01 {
 
+    int i;
     public static void main(String[] args) {
 
         System.out.println("Nikhil Jadhav Here ");
         System.out.println("Hello ");
+        System.out.println(" Integer i default value is :>"+i);
 
     }
 }
