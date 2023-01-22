602ae6f6975363c8b6d6d2bc7fbf42caa5266bbd
EngrNikhil <111607782+EngrNikhil@users.noreply.github.com>
2023-01-22 16:42:04 +0530

    Update practice01.java

--- PractiseNj/src/main/java/practice01.java
+++ PractiseNj/src/main/java/practice01.java
@@ -2,7 +2,7 @@ public class practice01 {
 
     int i;
     public static void main(String[] args) {
-
+        i=23;
         System.out.println("Nikhil Jadhav Here ");
         System.out.println("Hello ");
         System.out.println(" Integer i default value is :>"+i);
