2540373e5bfa494b1b1c89ecea4dd33744a4ca1f
Qa-Nikhil <Ernikhil.jadhav2306@gmail.com>
2023-01-22 18:08:15 +0530

    added 2nd java class

---
+++ PractiseNj/src/main/java/practice02.java
@@ -0,0 +1,11 @@
+public class practice02 {
+
+    public void getName(String nam){
+        System.out.println("name is :>"+nam);
+    }
+
+    public static void main(String[] args){
+        practice02 pr2=new practice02();
+        pr2.getName("Nikhil");
+    }
+}
