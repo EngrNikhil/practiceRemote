01dc25683cf7fb74016b0c2444ea78601870a434
Qa-Nikhil <Ernikhil.jadhav2306@gmail.com>
2023-01-27 23:32:50 +0530

    updated pom file

--- PractiseNj/src/main/java/practice01.java
+++ PractiseNj/src/main/java/practice01.java
@@ -3,11 +3,14 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 import java.time.Duration;
 
 public class practice01 {
 
-    public static void main(String[] args) {
+
+
+     public static void main(String[] args) {
         ChromeOptions co=new ChromeOptions();
         co.addArguments("--headless");
         ChromeDriver driver =new ChromeDriver(co);
@@ -15,5 +18,8 @@ public class practice01 {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Sangita Jadhav",Keys.ENTER);
+        System.out.println("Done");
+        driver.quit();
+
     }
 }
