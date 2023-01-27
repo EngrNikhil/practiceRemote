72e29ad7d83a0f530e1e066907f3745054cd2850
Qa-Nikhil <Ernikhil.jadhav2306@gmail.com>
2023-01-27 20:44:33 +0530

    Updated

--- PractiseNj/src/main/java/practice01.java
+++ PractiseNj/src/main/java/practice01.java
@@ -1,20 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class practice01 {
 
-    static int i;
-    String name ="Nikhil";
-    String Nj = "Jadhav";
     public static void main(String[] args) {
-        i=23;
-        System.out.println("Nikhil Jadhav Here ");
-        System.out.println("Hello ");
-        System.out.println(" Integer i default value is :>"+i);
-        i =94;
-        System.out.println("i value updated :>"+i);
-        practice01 pr =new practice01();
-        System.out.println("non static string name is :>"+pr.name);
-        System.out.println("Hello this is new branch Nj");
-        System.out.println("new nj string value is :>"+pr.Nj);
-        System.out.println("Hey Nikhil Jadhav");
+        ChromeDriver driver =new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Sangita Jadhav");
+        driver.findElement(By.xpath("//body")).sendKeys(Keys.ENTER);
 
     }
 }
