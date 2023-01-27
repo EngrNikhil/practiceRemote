f9e281d97c16e1d311beb330ac8abb3de2357594
Qa-Nikhil <Ernikhil.jadhav2306@gmail.com>
2023-01-27 21:59:15 +0530

    updated Headless driver

--- PractiseNj/src/main/java/practice01.java
+++ PractiseNj/src/main/java/practice01.java
@@ -1,13 +1,16 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 public class practice01 {
 
     public static void main(String[] args) {
-        ChromeDriver driver =new ChromeDriver();
+        ChromeOptions co=new ChromeOptions();
+        co.addArguments("--headless");
+        ChromeDriver driver =new ChromeDriver(co);
         driver.get("https://www.google.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
